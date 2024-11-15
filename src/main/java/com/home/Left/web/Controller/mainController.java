d869552585e14abaacd75d9cd199fea9777169b3
azx49 <azx49@DESKTOP-19KNIFH>
2024-11-15 14:56:05 +0900

    report 히스토리 기능 개발 시작

--- src/main/java/com/home/Left/web/Controller/mainController.java
+++ src/main/java/com/home/Left/web/Controller/mainController.java
@@ -1,17 +1,24 @@
 package com.home.Left.web.Controller;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.home.Left.Entity.depoWithHisObj;
 import com.home.Left.repository.depoWithHisRepository;
 import com.home.Left.repository.expHisRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class mainController {
@@ -32,22 +39,29 @@ public class mainController {
 		
 		List<depoWithHisObj> list = new ArrayList<depoWithHisObj>();
 		
-		depoWithHisRepository.findAll().forEach(t -> {
+		depoWithHisRepository.findByReportInfo("202411"+"31").forEach(t -> {
 			list.add(t);
 		});
 		
-		model.addAttribute("list", list);
+		model.addAttribute("meassage", list);
 		
 		return "/index.html";
 	}
 	
 	//report
 	@RequestMapping(method = RequestMethod.GET,value = "/report")
-	public String report(Model model) {
+	public String report(Model model,HttpServletRequest request) {
 		
 		List<depoWithHisObj> list = new ArrayList<depoWithHisObj>();
 		
-		depoWithHisRepository.findAll().forEach(t -> {
+		String param = request.getParameter("date");
+		
+		if(!StringUtils.hasLength(param)) {
+			param = LocalDate.now(ZoneId.of("Asia/Tokyo")).format(DateTimeFormatter.ofPattern("yyyyMM"));
+		}
+		
+		
+		depoWithHisRepository.findByReportInfo(param+"31").forEach(t -> {
 			list.add(t);
 		});
 		
